d0797ec638438ec9ca486ead6b3e144248d40c66
vegedra <pedroivo0513@gmail.com>
2024-09-15 19:27:46 -0300

    exportado de forma automatica em 15-09-2024 19:27

--- make.bat
+++ make.bat
@@ -1,4 +1,10 @@
 ﻿@echo off
 cls
-python main.py
+echo 1) sem TUI
+echo 2) com TUI
+choice /c 12 /n
+
+if %errorlevel% EQU 1 python main_semTUI.py
+if %errorlevel% EQU 2 python main_comTUI.py
+
 pause
